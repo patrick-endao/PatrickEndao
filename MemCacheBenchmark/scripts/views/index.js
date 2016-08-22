5589f899c97e5c8d877b76f0955dd0183c13b95c
patrick-endao <patrick@endao.com>
2016-08-22 08:46:39 -0500

    Refactored client scripting

--- MemCacheBenchmark/scripts/views/index.js
+++ MemCacheBenchmark/scripts/views/index.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function () {
     handleFormSubmission();
-})
+});
 
 function handleFormSubmission() {
     $('#benchmarkConfigurationForm').submit(function (event) {
@@ -8,7 +8,6 @@ function handleFormSubmission() {
         var formValues = getFormValues();
         var benchmarkParameters = unpackSerializedArray(formValues);
         launchBenchmark(benchmarkParameters);
-        switchToLog();
     });
 }
 
@@ -38,75 +37,58 @@ function launchBenchmark(benchmarkParameters) {
 }
 
 var Benchmark = function (benchmarkParameters) {
-    this.numberOfObjects = parseInt(benchmarkParameters.objNum);
-    this.numberOfQueries = parseInt(benchmarkParameters.objQueries);
-    this.minimumObjectDepth = parseInt(benchmarkParameters.minDepth);
-    this.maximumObjectDepth = parseInt(benchmarkParameters.maxDepth);
-    this.minimumObjectScope = parseInt(benchmarkParameters.minScope);
-    this.maximumObjectScope = parseInt(benchmarkParameters.maxScope);
-    this.benchmarkResults = new BenchmarkResultsReport();
-    this.timer = new Timer();
+    this.RADIX = 10;
+    this.parameters = benchmarkParameters;
     this.logger = new Logger();
+    this.scheduler = new AjaxScheduler();
+    this.benchmarkResults = new BenchmarkResultsReport();
+    this.numberOfObjects = parseInt(this.parameters.objNum, this.RADIX);
+    this.numberOfQueries = parseInt(this.parameters.objQueries, this.RADIX);
     this.objectIndex = 0;
 
-    this.runTests = function(){
+    this.runTests = function () {
         this.runCachelessTests();
         this.runCacheTests();
-    }
+    };
 
     this.runCachelessTests = function () {
         for (i = 0; i < this.numberOfObjects; i++) {
             this.objectIndex = i;
-            this.logger.logMessage('Running queries for object #' + String(i+1));
-            this.runQueriesForObject();
+            this.logger.logMessage('Running queries for object #' + String(i + 1));
+            var ajaxRequest = this.packageRequest();
+            this.scheduler.scheduleAjaxCalls(ajaxRequest, this.numberOfQueries, this.benchmarkResults);
         }
-    }
-
-    this.runQueriesForObject = function () {
-        for (j = 0; j < this.numberOfQueries; j++) {
-            this.queryServer();
-        }
-    }
-
-    this.queryServer = function () {
-        this.timer.start();
-        this.sendGetRequest();
-        this.timer.stop();
-        var elapsedTime = this.timer.getTotal();
-        this.benchmarkResults.insertServerResultRecord(elapsedTime);
-    }
-
-    this.sendGetRequest = function () {
-        $.ajax({
-            url:"/Home/GenerateTestObject",
-            type: "GET",
-            data: this.packageRequest(),
-            dataType: "json",
-            success: function (response) {
-                this.logger.logMessage(JSON.stringify(response));
-            },
-            error: function (error) {
-                alert(JSON.stringify(error));
-            }
-        })
-    }
+    };
 
     this.packageRequest = function () {
-        request = { objIndex : this.objectIndex,
-                    minObjDepth : this.minimumObjectDepth,
-                    maxObjDepth : this.maximumObjectDepth,
-                    minObjScope : this.minimumObjectScope,
-                    maxObjScope: this.maximumObjectScope }
+        var minimumObjectDepth = parseInt(this.parameters.minDepth, this.RADIX);
+        var maximumObjectDepth = parseInt(this.parameters.maxDepth, this.RADIX);
+        var minimumObjectScope = parseInt(this.parameters.minScope, this.RADIX);
+        var maximumObjectScope = parseInt(this.parameters.maxScope, this.RADIX);
+        request = {
+            objIndex: this.objectIndex,
+            minObjDepth: minimumObjectDepth,
+            maxObjDepth: maximumObjectDepth,
+            minObjScope: minimumObjectScope,
+            maxObjScope: maximumObjectScope
+        };
         return request;
-    }
+    };
+
+    this.recordQueryResults = function (response, elapsedTime) {
+        report = this.benchmarkResults;
+        report.insertServerObject(this.objectIndex, response);
+        report.insertServerResultRecord(elapsedTime);
+        report.incrementSucceeded();
+    };
 
     this.runCacheTests = function () {
 
-    }
+    };
 
-    this.getResults = function(){
+    this.getResults = function () {
         return this.benchmarkResults;
-    }
+    };
 };
 
 var BenchmarkResultsReport = function(){
@@ -114,6 +96,8 @@ var BenchmarkResultsReport = function(){
     this.cacheResults = [];
     this.serverObjects = {};
     this.cacheObjects = {};
+    this.succeededCalls = 0;
+    this.failedCalls = 0;
 
     this.insertServerResultRecord = function (result) {
         this.serverResults.push(result);
@@ -123,32 +107,95 @@ var BenchmarkResultsReport = function(){
         this.cacheResults.push(result);
     };
 
-    this.getServerResults = function(){
-        return this.serverResults
-    }
+    this.insertServerObject = function (objectIndex, serverObject) {
+        this.serverObjects[objectIndex] = serverObject;
+    };
 
-    this.getCacheResults = function(){
-        return this.cacheResults
+    this.insertCacheObject = function (objectIndex, serverObject) {
+        this.cacheObjects[objectIndex] = serverObject;
+    };
+
+    this.getServerResults = function () {
+        return this.serverResults;
+    };
+
+    this.getCacheResults = function () {
+        return this.cacheResults;
+    };
+
+    this.incrementSucceeded = function () {
+        this.succededCalls++;
+    };
+
+    this.incrementFailed = function () {
+        this.failedCalls++;
+    };
+
+    this.isReady = function () {
+
     }
 };
+
+var AjaxScheduler = function () {
+
+    this.scheduleAjaxCalls = function (packagedRequest, numberOfQueries, benchmarkResults) {
+        this.packagedRequest = packagedRequest;
+        this.queriesRemaining = numberOfQueries;
+        this.results = benchmarkResults;
+        this.elapsedQueryTimes = [];
+        this.serverResponses = [];
+        var scheduler = this;
+        this.sendAjaxCall(scheduler);
+    };
+
+    this.sendAjaxCall = function (scheduler) {
+        scheduler.queriesRemaining -= 1;
+        var remainder = scheduler.queriesRemaining;
+        var timer = new Timer();
+        timer.start();
+        $.ajax({
+            url: "/Home/GenerateTestObject",
+            type: "GET",
+            data: scheduler.packagedRequest,
+            dataType: "json",
+            success: function (response) {
+                timer.stop();
+                elapsedTime = timer.getTotal();
+                console.warn(String(elapsedTime));
+                scheduler.recordQueryResults(response, elapsedTime);
+                if (scheduler.queriesRemaining > 0) {
+                    setTimeout(scheduler.sendAjaxCall(scheduler), 100);
+                }
+            },
+            error: function (error) {
+                console.warn('Error' + JSON.stringify(error));
+            }
+        });
+    };
+
+    this.recordQueryResults = function (response, elapsedTime) {
+        this.elapsedQueryTimes.push(elapsedTime);
+        this.serverResponses.push(response);
+    };
+}
 
 var Timer = function(){
     this.startTime = 0;
     this.endTime = 0;
     this.elapsedTime = 0;
 
-    this.start = function(){
+    this.start = function () {
         this.startTime = new Date();
-    }
+    };
 
-    this.stop = function(){
-        this.endTime = new Date();    
-    }
+    this.stop = function () {
+        this.endTime = new Date();
+    };
 
-    this.getTotal = function(){
+    this.getTotal = function () {
         this.elapsedTime = this.endTime - this.startTime;
         return this.elapsedTime;
-    }
+    };
 };
 
 var Logger = function () {
@@ -157,7 +204,7 @@ var Logger = function () {
     this.logMessage = function (message) {
         this.console.innerHTML = this.console.innerHTML + '<br>' + message;
     };
-}
+};
 
 function switchActiveTab(tabClass) {
     $('.nav-pills a[href="' + tabClass + '"]').tab('show');
